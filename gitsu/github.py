cf9588df9934bdde0a8fdc0cb9aa6e66a39b4c09
Dylan <>
2019-02-04 07:01:54 -0700

    added github helper functions for requests

--- gitsu/github.py
+++ gitsu/github.py
@@ -17,33 +17,69 @@ github_client = (
 )
 
 
+# def repo_events(repo, number=-1, etag=None, **params):
+#     url = repo._build_url('events', base_url=repo._api)
+#     params = repo_issue_params(milestone, state, assignee, mentioned,
+#                                labels, sort, direction, since)
+#     return repo._iter(int(number), url, events.Event, params=params, etag=etag)
+# 	pass 
+
+# def repo_issue_params(milestone=None,
+#                       state=None,
+#                       assignee=None,
+#                       mentioned=None,
+#                       labels=None,
+#                       sort=None,
+#                       direction=None,
+#                       since=None,
+#                       number=-1,
+#                       etag=None):
+#     """Validate and filter issue method parameters in one place."""
+#     params = {'assignee': assignee, 'mentioned': mentioned}
+#     if milestone in ('*', 'none') or isinstance(milestone, int):
+#         params['milestone'] = milestone
+#     Repository._remove_none(params)
+#     params.update(
+#         issues.issue_params(
+#             None, state, labels, sort, direction, since
+#         )
+#     )
+#     return params
+
+
+
+
 def github_default_callback(item):
-	return item.as_dict()
+	return item
 
 
-def github_default_page_callback(iterator, item):
-	if hasattr(item, 'created_at'):
-		logging.info('{} created_at {}'.format(item, item.created_at))
+def github_default_new_page_callback(iterator, item):
+	if hasattr(item, 'updated_at'):
+		logging.info('{} updated_at {}'.format(item, item.updated_at))
 
 
-def github_iterator_results(iterator, limit=None, callback=github_default_callback, page_callback=github_default_page_callback):
+def github_iterator_results(iterator, limit=None, callback=github_default_callback, new_page_callback=github_default_new_page_callback):
 	results = []
 	page_cnt = 0
 	prev_req_url = None
-	for item in iterator:
-	    # if the page changed, log progress information
-	    req_url = iterator.last_response.url
-	    if req_url != prev_req_url:
-	        prev_req_url = req_url
-	        page_cnt += 1
-	        logging.info('Pagination {}'.format(page_cnt))
-	        logging.info(req_url)
-	        page_callback(iterator, item)
+	try:
+		for item in iterator:
+		    # if the page changed, log progress information
+		    req_url = iterator.last_response.url
+		    if req_url != prev_req_url:
+		        prev_req_url = req_url
+		        page_cnt += 1
+		        logging.info('Pagination {}'.format(page_cnt))
+		        logging.info('rate limit remaining {}'.format(getattr(iterator, 'ratelimit_remaining', -1)))
+		        logging.info(req_url)
+		        new_page_callback(iterator, item)
 
-	    # append the issuees
-	    results.append(callback(item))
-	    if limit and len(results) > limit:
-	    	break
+		    # append the issuees
+		    results.append(callback(item))
+		    if limit and len(results) > limit:
+		    	break
+	except Exception as e:
+		logging.error(str(e))
 	return results
 
 
