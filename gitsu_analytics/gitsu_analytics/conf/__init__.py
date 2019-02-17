83106a68e84cad64c54bfc1fca48eec7b483368a
Dylan <>
2019-02-17 13:26:30 -0700

    refactored settings into yaml files

--- gitsu_analytics/gitsu_analytics/conf/__init__.py
+++ gitsu_analytics/gitsu_analytics/conf/__init__.py
@@ -1,25 +1,138 @@
-import os 
-import importlib
+import os
+import sys
+import yaml
 import logging
 import logging.config
-
-settings_module_name = os.environ.get('SETTINGS_MODULE', None)
-if settings_module_name is None:
-    try:
-        settings_module_name = 'local_settings'
-        settings = importlib.import_module(settings_module_name)
-    except ImportError:
-        settings_module_name = 'gitsu_analytics.conf.base'
-        settings = importlib.import_module(settings_module_name)
-else:
-    settings = importlib.import_module(settings_module_name)        
+import warnings
 
 
-if settings.CONFIGURE_LOGGING:
-    if isinstance(settings.LOGGING_CONFIG, str):
-        logging.basicConfig(filename=settings.LOGGING_CONFIG)
-    else:
-        logging.config.dictConfig(settings.LOGGING_CONFIG)
-        
-    if settings.LOGGING_LEVEL is not None:
-        logging.root.setLevel(settings.LOGGING_LEVEL)
+CONFIG_PATH = os.environ.get(
+    'CONFIG_PATH',
+    os.path.dirname(__file__),
+)
+
+
+CONFIG_FILES = os.environ.get(
+    'CONFIG_FILES',
+    'base.yaml:local_settings.yaml',
+)
+
+
+def _update_relpaths(config, key_suffix='_PATH'):
+    """ Updates PATH variables to be relative to server
+
+    Note:
+      - MODULE_ROOT_PATH is implied if not overwritten. 
+        The implied value is os.path.dirname(__file__)
+
+    Example:
+
+        PROJECT_PATH: '{MODULE_ROOT_PATH}/resources'
+        settings.PROJECT_PATH == '/opt/path/module/resources'
+    """
+    for key, value in config.items():
+        if key.endswith(key_suffix):
+            relpath = value.format(**config)
+            config[key] = os.path.abspath(relpath)
+
+
+def _resolve_config_filepaths(config_files=None, config_path=None):
+    """ Resolves which filepath to load config from
+
+    For each filename in config_files, look in the config_path 
+    and locate the first.
+
+    """
+    # default config path
+    if config_path is None:
+        config_path = CONFIG_PATH
+    if isinstance(config_path, str):
+        config_path = config_path.split(':')
+
+    # default config files
+    if config_files is None:
+        config_files = CONFIG_FILES
+    if isinstance(config_files, str):
+        config_files = config_files.split(':')
+
+    config_filepaths = []
+    # get filename
+    for fn in config_files:
+        found = False
+        logging.debug('looking for config file "{}"'.format(fn))
+
+        # get path
+        for path in config_path:
+            logging.debug('looking in path "{}"'.format(path))
+
+            # join into filepath
+            fp = os.path.join(path, fn)
+            found = os.path.exists(fp)
+            if found:
+                logging.info('Found config "{}"'.format(fp))
+                break
+        if found:
+            config_filepaths.append(fp)
+        else:
+            warning = (
+                '\nConfig file not found "{}" in {}'
+                .format(fn, config_path)
+            )
+            warnings.warn(warning)
+    return config_filepaths
+
+
+def _load_config(config_filepaths):
+    """ Load config files with inheritance
+
+    Load each config file and update the result dictionary.
+
+    """
+    # Base Settings
+    config = {}
+    config['MODULE_ROOT_PATH'] = os.path.dirname(
+        os.path.dirname(__file__)
+    )
+
+    for fp in config_filepaths:
+        with open(fp) as f:
+            config.update(yaml.load(f))
+
+    _update_relpaths(config, '_PATH')
+    _update_relpaths(config, '_FILEPATH')    
+    return config
+
+
+class Settings:
+
+    def __init__(self, config_files=None, config_path=None):
+        kws = dict(
+            config_files=config_files, config_path=config_path
+        )
+        config_filepaths = _resolve_config_filepaths(**kws)
+        self.__dict__ = {
+            'CONFIG_FILEPATHS': config_filepaths,
+        }
+        self.__dict__.update(
+            _load_config(config_filepaths)
+        )
+        self._configure_logging()
+
+    def _reload(self, **kws):
+        self = Settings(**kws)
+
+    def _configure_logging(self):
+        logging_config = getattr(self, 'LOGGING_CONFIG')
+        if not logging_config:
+            return 
+        if isinstance(logging_config, str):
+            logging.basicConfig(filename=logging_config)
+        else:
+            logging.config.dictConfig(logging_config)
+
+
+# =========================================================================== #
+# Load Settings
+# =========================================================================== #
+
+settings = Settings()
