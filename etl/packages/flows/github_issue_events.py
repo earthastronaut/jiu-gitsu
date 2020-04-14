e96058786489bef4bac18444b49b19f201d9f12a
earthastronaut <>
2020-04-13 18:43:55 -0600

    moved scripts to tasks and created initial flows

---
+++ etl/packages/flows/github_issue_events.py
@@ -0,0 +1,3 @@
+# 1. download_github_issue_events.py
+# 1. etl_github_users.py
+# 1. etl_github_issue_events.py
---
+++ etl/packages/flows/github_issues.py
@@ -0,0 +1,4 @@
+# 1. download_github_issues.py
+# 1. etl_github_repo.py
+# 1. etl_github_users.py
+# 1. etl_github_issues.py
